11d6c59a2a66020187143dcd41dd3ab1625b5c6f
D__ <denglian1008@163.com>
2018-06-03 22:38:41 +0800

    pre-commit test

--- test/timer.spec.ts
+++ test/timer.spec.ts
@@ -3,7 +3,3 @@ import { Timer } from '../src';
 test('timer helloWorld', () => {
   expect(new Timer().helloWorld()).toBe('helloWorld');
 });
-
-test('timer helloWorld2', () => {
-  expect(new Timer().helloWorld()).toBe('helloWorld2');
-});
