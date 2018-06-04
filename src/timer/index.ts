8f9b00550589d27eeff5d01d401830dddd4095ee
D__ <denglian1008@163.com>
2018-06-04 10:02:29 +0800

    timer

--- src/timer/index.ts
+++ src/timer/index.ts
@@ -1,9 +1,89 @@
+
 export default class Timer {
-  async getDate() {
-    return new Date();
+  static MS_SECOND = 1000;
+  static MS_MINUTE = Timer.MS_SECOND * 60;
+  static MS_HOURE = Timer.MS_MINUTE * 60;
+  static MS_DAY = Timer.MS_HOURE * 24;
+
+  _value: Date = null;
+
+  constructor(date?: Date | Number | string | Timer | null) {
+    if (date instanceof Date) {
+      // assign new Date to toDate when toDate is Invalid Date
+      if (isNaN(date.getTime())) {
+        this.setValue(new Date());
+      } else {
+        this.setValue(date);
+      }
+    }
+
+    if (typeof (date) === 'number' || typeof (date) === 'string') {
+      let toDate = new Date(date);
+      // assign new Date to toDate when toDate is Invalid Date
+      if (isNaN(toDate.getTime())) {
+        toDate = new Date();
+      }
+      this.setValue(toDate);
+    }
+
+    if (date instanceof Timer) {
+      this.setValue(date.getValue());
+    }
+
+    if (typeof (date) === 'undefined' || date === null) {
+      this.setValue(new Date());
+    }
   }
 
-  helloWorld() {
-    return 'helloWorld';
+  getTime(): Number {
+    return this.getValue().getTime();
+  }
+
+  getFirstDayOfMonth(): Timer {
+    let firstDayOfMonth = new Date(this.getValue());
+    firstDayOfMonth.setDate(1);
+    firstDayOfMonth.setHours(0);
+    firstDayOfMonth.setMinutes(0);
+    firstDayOfMonth.setSeconds(0);
+    return new Timer(firstDayOfMonth);
+  }
+
+  getLastDayOfMonth() {
+    let year = this.getValue().getFullYear();
+    let month = this.getValue().getMonth();
+    let nexMonth = month + 1;
+    let nexMonthFirthDay = new Date(year, nexMonth, 1);
+    let lastDayOfMonth = new Date(nexMonthFirthDay.getTime() - Timer.MS_DAY);
+    return new Timer(lastDayOfMonth);
+  }
+
+  format(fmt: string) {
+    let _fmt = fmt;
+    let date = this.getValue();
+    let o = {
+      'M+': date.getMonth() + 1, //月份
+      'd+': date.getDate(), //日
+      'h+': date.getHours(), //小时
+      'm+': date.getMinutes(), //分
+      's+': date.getSeconds(), //秒
+      'q+': Math.floor((date.getMonth() + 3) / 3), //季度
+      'S': date.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(_fmt)) _fmt = _fmt.replace(RegExp.$1, (String(date.getFullYear())).substr(4 - RegExp.$1.length));
+    for (let k in o) {
+      if (new RegExp('(' + k + ')').test(_fmt)) {
+        _fmt = _fmt
+          .replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr((String(o[k])).length)));
+      }
+    }
+    return _fmt;
+  }
+
+  setValue(value: Date) {
+    this._value = value;
+  }
+
+  getValue(): Date {
+    return this._value;
   }
 }
--- test/timer.spec.ts
+++ test/timer.spec.ts
@@ -1,5 +1,29 @@
 import { Timer } from '../src';
 
-test('timer helloWorld', () => {
-  expect(new Timer().helloWorld()).toBe('helloWorld');
+test('getTime', () => {
+  let dateString = '2018/06/02';
+  expect(new Timer(dateString).getTime()).toBe(new Date(dateString).getTime());
+});
+
+test('format', () => {
+  let dateString = '2018/06/02';
+  expect(new Timer(dateString).format('yyyy/MM/dd')).toBe(dateString);
+});
+
+test('new Timer', () => {
+  let dateString = '2018/06/02';
+  let date = new Date(dateString);
+
+  expect(new Timer(dateString).format('yyyy/MM/dd')).toBe(dateString);
+  expect(new Timer(date).format('yyyy/MM/dd')).toBe(dateString);
+});
+
+test('getFirstDatOfMonth', () => {
+  let dateString = '2018/06/02';
+  expect(new Timer(dateString).getFirstDayOfMonth().format('yyyy/MM/dd')).toBe('2018/06/01');
+});
+
+test('getLastDateOfMonth', () => {
+  let dateString = '2018/06/02';
+  expect(new Timer(dateString).getLastDayOfMonth().format('yyyy/MM/dd')).toBe('2018/06/30');
 });
